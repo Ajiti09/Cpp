9a6133708b7dc923e495b22cbd04f2e652233953
Ajiti09 <ajitishaw948@gmail.com>
2026-04-10 23:59:48 +0530

    Rectified

--- 2DArrays/SearchSortedMatrix.cpp
+++ 2DArrays/SearchSortedMatrix.cpp
@@ -1,17 +1,43 @@
 #include<iostream>
 using namespace std;
+//Brute Force
 int search(int arr[][4],int n,int m,int key){
     bool isfound=false;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(key == arr[i][j]){
                 cout<<"Row:"<<i;
-                cout<<"Column:"<<j; 
+                cout<<"Column:"<<j;
                 isfound=true;
             }
         }
     }
     if(!isfound){
+    cout<<"not found";}
+}
+//Optimized BS 
+void search2(int arr[][4],int n,int m,int key){
+    int srow=0,erow=n-1;  
+    bool isfound=false;
+    for(int i=srow;i<=erow;i++){
+        int scol=0,ecol=m-1;
+    while(scol<=ecol){
+        int mid=(scol+ecol)/2;
+        if(arr[i][mid]==key){
+            cout<<"Row:"<<i;
+            cout<<"Column:"<<mid;
+                isfound=true;
+                break;
+        }
+        else if(arr[i][mid]<key){
+            scol=mid+1;
+        }
+        else{
+            ecol=mid-1;
+        }
+    }
+}
+if(!isfound){
     cout<<"not found";}
 }
 int main(){
@@ -22,6 +48,6 @@ int main(){
     int key,row,column;
     cout<<"enter key:";
     cin>>key;
-    search(arr,4,4,key);
+    search2(arr,4,4,key);
     
 }
